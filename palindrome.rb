bbb71eb5ae98d50a141fd360bf3f30b09c2fc4f4
Boris Paillard <boris.paillard@gmail.com>
2013-11-05 17:42:54 +0100

    puts first challenge palindrome as an example

---
+++ palindrome.rb
@@ -0,0 +1,11 @@
+def palindrome?(a_string)
+  # return true if a_string is a palindrome / false otherwise 
+end
+
+  
+def main 
+  # Testing palindrome? function
+  puts "#{palindrome?("A man, a plan, a canal -- Panama")}" #=> true
+  puts "#{palindrome?("Madam, I'm Adam!")}" # => true
+  puts "#{palindrome?("Abracadabra")}" # => false  
+end
