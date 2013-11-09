4abc6d1c7c43e6d294f33739d54b4f4b3756178e
mathieuripert <mathieu.ripert@gmail.com>
2013-11-09 11:03:21 -0800

    simple_looping exercice

---
+++ simple_looping.rb
@@ -0,0 +1,17 @@
+# The goal of this exercice is to learn how to make a simple loop
+# The objective is to create a function that computes the sum of the integers from a min value to a max value
+
+def sum(min,max)
+  # your code here
+end
+
+
+
+# tests
+
+min = 1
+max = 100
+
+sum = sum(1,100)
+# sum should be equal to 5050
+puts sum
