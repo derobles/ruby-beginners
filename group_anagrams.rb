b0eabce5be7af1ade57fade3c4eb48c559594b1a
Papillard <boris.paillard@gmail.com>
2013-11-27 11:22:04 +0100

    Update group_anagrams.rb

--- group_anagrams.rb
+++ group_anagrams.rb
@@ -5,7 +5,8 @@ end
 # input:
 group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
-# => output: [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
+# output:
+# =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
 
 # HINT: you can quickly tell if two words are anagrams by sorting their
 # letters, keeping in mind that upper vs lowercase doesn't matter
