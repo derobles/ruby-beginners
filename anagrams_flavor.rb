375b635095658f5927f75cd1dceaeed921dbf776
Papillard <boris.paillard@gmail.com>
2013-11-28 10:57:35 +0100

    Create anagrams_flavor.rb

---
+++ anagrams_flavor.rb
@@ -0,0 +1,14 @@
+def anagrams?( a_string, another_string )
+  # your code goes here
+end
+
+def anagrams_on_steroids?( a_string, another_string )
+  # your code goes here
+end
+
+# O[nlog(n)]
+puts anagrams? "Monica Lewinsky", "Nice silky woman" # => true 
+
+# O[n]
+puts anagrams_on_steroids? "Monica Lewinsky", "Nice silky woman" # => true 
+
