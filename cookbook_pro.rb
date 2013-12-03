ec518d3d29fd3a486760a908de6586e0635b3c1b
Papillard <boris.paillard@gmail.com>
2013-12-03 11:58:32 +0100

    Create cookbook_pro.rb

---
+++ cookbook_pro.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+title = 'Recettes de kiffeur'
+
+chapters = [
+             ['Materiel pour cuisiner', 1],
+             ['Produits de saison', 8],
+             ['Sauces et soupes', 19],
+             ['Viandes et Gibiers', 38],
+             ['Poissons, coquillages et crustaces', 157]
+           ]
+           
+           
+# your program goes here..
