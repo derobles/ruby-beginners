e38b2d0ccf6c14747238d322a899aa48372e3b86
Papillard <boris.paillard@gmail.com>
2013-12-04 16:11:43 +0100

    Create refactor_gym_tonic.rb

---
+++ refactor_gym_tonic.rb
@@ -0,0 +1,16 @@
+# Very dirty code to make some dirty gym...
+def hop_hop_hop( number_of_exercises )
+  (1..number_of_exercises).each do |i| 
+    counter = 0
+    while counter < i do
+      print "hop! "
+      counter += 1
+    end
+    if counter == i # Test if reaching the end of the exercise.
+      # Prints cutom message in that case    
+      puts "Encore une fois.."
+    end  
+    # Reset counter to 0 for the nex exercise
+    counter = 0
+  end
+end
